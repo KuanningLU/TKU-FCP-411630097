ebbb9b555a49c5494e429689182a95549211a2bd
KuanningLU <113368082+KuanningLU@users.noreply.github.com>
2024-07-02 11:53:42 +0800

    Add files via upload

---
+++ Ch2_1.java
@@ -0,0 +1,13 @@
+//Ch2_1，簡單的JAVA程式
+public class Ch2_1{         //定義public類別Ch2_1
+    public static void main(String[] args){  //main()函數。主程式開始
+        int num1=3;  //宣告整數num
+        int num2=5;
+        float num3=4;
+        String num4="五";
+        System.out.println(num1+"cats are running");  //印出字串及變數內容
+        System.out.println(num2+"dogs are running");
+        System.out.println(num3+"birds are running");
+        System.out.println(num4+"dogs are running");
+    }
+}
