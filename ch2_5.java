796ba13afb6d71f371ee0213bbec6c0e2d034fa3
KuanningLU <411630097@o365.tku.edu.tw>
2024-07-03 11:42:31 +0800

    新增ch2_4,ch2_5，ch2_6

---
+++ ch2_5.java
@@ -0,0 +1,10 @@
+public class ch2_5 {
+    public static void main(String[] args) {
+        int a=2; //宣告整數變數a,並設值為2
+        int b=3; //宣告整數變數b,並設值為2
+        int c=a+b; //宣告整數變數c,並設值為a+b
+
+        System.out.println("a+b="+c); //印出字串""，印出C，另一方法+("a+b="+(a+b))
+    }
+    
+}
