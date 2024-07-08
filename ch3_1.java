b3e2c57ee0e96e0c850fc5ab12440729c213d2fa
KuanningLU <411630097@o365.tku.edu.tw>
2024-07-08 11:09:46 +0800

    新增ch3_1,2,3,4

---
+++ ch3_1.java
@@ -0,0 +1,9 @@
+public class ch3_1 {
+    public static void main(String[] args) {
+        int num=3;
+        char ch='C';
+        System.out.println(num+" is an unteger");
+        System.out.println(num+" is a cgaracter");
+    }
+    
+}
---
+++ ch3_3.java
@@ -0,0 +1,16 @@
+public class ch3_3 {
+    public static void main(String[] args) {
+        byte bin=0b1111;
+        short oct=02001;
+        int hext1=0xff12;
+        long hex2=0x12345ffeL;
+
+        System.out.println("bin= " + bin);
+        System.out.println("oct= " + oct);
+        System.out.println("hex1= " + hext1);
+        System.out.println("hex2= " + hex2);
+
+        
+    }
+    
+}
