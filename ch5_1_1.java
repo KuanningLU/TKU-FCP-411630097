c727fd81e5649e09bc38b62e59b89a72abc0855d
KuanningLU <411630097@o365.tku.edu.tw>
2024-07-15 13:34:12 +0800

    新增5_1_1~11
    新增5_1_1~11

---
+++ ch5_1_1.java
@@ -0,0 +1,11 @@
+public class ch5_1_1 {
+    public static void main(String[] args) {
+        int i,sum=0;
+        
+        for(i=1;i<=100;i++)
+            sum+=i;
+        System.out.printf("1+2+....+100=%d", sum);
+
+    }
+    
+}
